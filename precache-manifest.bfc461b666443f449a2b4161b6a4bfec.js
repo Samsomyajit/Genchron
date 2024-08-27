cc5a3fcfc929da8b0812bad92c2ed5d63df84ac5
Samsomyajit <somyajitchppr@gmail.com>
2024-08-28 00:56:08 +0100

    Updates

---
+++ precache-manifest.bfc461b666443f449a2b4161b6a4bfec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e152633935f007f4f4b4e7b627fc7521",
+    "url": "/Genchron/index.html"
+  },
+  {
+    "revision": "84451b68677ac53f8231",
+    "url": "/Genchron/static/css/main.d887b7ca.chunk.css"
+  },
+  {
+    "revision": "206ace865bc0bc0d27ea",
+    "url": "/Genchron/static/js/2.ac2f3c3e.chunk.js"
+  },
+  {
+    "revision": "d8a9afcabafd1174b61323eb2471cf4b",
+    "url": "/Genchron/static/js/2.ac2f3c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f052d528b3025b6523b4",
+    "url": "/Genchron/static/js/3.75ea4ae7.chunk.js"
+  },
+  {
+    "revision": "84451b68677ac53f8231",
+    "url": "/Genchron/static/js/main.ca67e364.chunk.js"
+  },
+  {
+    "revision": "0a91d66959184ee63843",
+    "url": "/Genchron/static/js/runtime-main.9a7c19ae.js"
+  },
+  {
+    "revision": "83434d0cb05b4e1e2d609f3e85e4811b",
+    "url": "/Genchron/static/media/Background.83434d0c.jpg"
+  },
+  {
+    "revision": "7ccfbf442d3a17b930f125345fc2b34c",
+    "url": "/Genchron/static/media/Olicana-Fine.7ccfbf44.ttf"
+  }
+]);
